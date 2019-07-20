6739c00eb942fc5a8cd1d5224cfb3ad4f36d4dca
Anton N <nesterow@Antons-iMac.local>
2019-07-19 20:41:49 -0700

    [client] + init hooks

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,25 @@
-require('plugins')
-const riot = require('riot')
-const {assign, debounce} = require('lodash')
-const hydrate = require('@riotjs/hydrate')
-const EventBus = require('eventbusjs')
-const Turbolinks = require('turbolinks')
+// 1. import global libraries like JQuery after this line
 
-Turbolinks.start()
+import start from '@frontless/core/start'
 
+const PAGES = require('./**/*.riot', {mode: 'list'});
+const MODULES = require('../components/**/*.riot', {mode: 'list'});
+const components = PAGES.concat(MODULES)
 
-riot.install(function(component){
+start({ 
+  components: components,
 
-  component.onServerState = function (response) {
-    this.state = assign(this.state, response.target.result.data)
-    this.update()
-  }.bind(component)
+  before() {
+    // 2. Do somethig before app is hydrated
 
-  const onMounted = component.onMounted || function () {}.bind(component)
-  const eventName = (component.id || component.name) + ':update'
+    require('plugins')
+    return Promise.resolve()
+  },
 
-  component.onMounted = function (props, state) {
-    EventBus.removeEventListener(eventName, component.onServerState.bind(this), component)
-    EventBus.addEventListener(eventName, component.onServerState.bind(this), component)
-    if (component.onRendered) {
-      setTimeout(component.onRendered.bind(component))
-    }
-    return onMounted.bind(this)(props, state)
-  }.bind(component);
-  
-
-})
-
-
-const tags = require('./**/*.riot', {mode: 'list'})
-  .concat(require('../components/**/*.riot', {mode: 'list'}))
-  
-const xss = require('xss')
-const initialize = () => {
- 
-  
-  tags.forEach((tag) => {
+  after() {
     
-    const component = tag.module.default
-    
-    if (component.exports) {
+    // 3. Perform actions after application is hydrated
+    // require('components/vendor')
 
-      let initilaExports = typeof component.exports === 'function' ? component.exports() : component.exports;
-      
-      component.exports = () => {
-        const STATE = JSON.parse(
-          xss.filterXSS(document.getElementById('state').innerText)
-        )
-      
-        const ATTRS = JSON.parse(
-          xss.filterXSS(document.getElementById('attributes').innerText)
-        )
-        initilaExports.state = STATE [initilaExports.id || component.name] || initilaExports.state;
-        const attributes = ATTRS [initilaExports.id || component.name]
-        if (attributes) attributes.map((attr) => {
-          initilaExports[attr.name] = attr.data
-        })
-        return initilaExports
-      }
-    }
-    try { 
-      riot.register(component.name, component)
-    } catch(e) {}
-  })
-
-  const root = document.querySelector('section[is]')
-  if (root) {
-    document.body.classList.add('disabled')
-    const ComponentImplementation = tags.find((tag) => tag.module.default.name === root.getAttribute('is') )
-    const component = ComponentImplementation.module.default;
-    hydrate(component)(root)
-    setTimeout(() => document.body.classList.remove('disabled'))
-  }
-
-}
-window.initialize = initialize;
-document.addEventListener('turbolinks:load', () => {
-  const fromCache = !!document.body.getAttribute('from-cache')
-  if (!fromCache) {
-    initialize()
   }
 });
-
-document.addEventListener('hmr:updated', () => setTimeout(() => location.reload(), 1800))
