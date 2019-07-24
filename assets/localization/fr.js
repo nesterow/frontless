7d4dad4ee51a8c711b516020401c8faa80c2036e
Anton Nesterov <arch.nesterov@gmail.com>
2019-07-24 05:54:26 +0300

    [docs]

---
+++ assets/localization/fr.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){localize({"header form input&placeholder":"Rechercher...","#pagination .previous span&html":"Précédent","#pagination .next span&html":"Suivant",".links > :eq(0) a&html":"Faire un fork sur Github",".links > :eq(1) a&html":"Mettre une étoile sur Github","#navbar footer&html":'Propulsé par <a href="https://skydocs.skyost.eu">SkyDocs</a> v0.6 Bêta.'})});
